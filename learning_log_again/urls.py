96ee69fae67d9b8d86c9ede90ecef04ab5fcf213
roman <yummison44@gmail.com>
2023-03-23 20:50:19 +0100

    implemented topics page

--- learning_log_again/urls.py
+++ learning_log_again/urls.py
@@ -6,4 +6,5 @@ app_name = 'learning_log_again'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
 ]
--- learning_log_again/views.py
+++ learning_log_again/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     # The home page for Learning Log again #
     return render(request, 'learning_log_again/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_log_again/topics.html', context)
