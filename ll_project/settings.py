e9368871920448ff6bac92755a247950337e9528
roman <yummison44@gmail.com>
2023-03-23 18:35:09 +0100

    making a first page and template

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'learning_log_again',
+
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
