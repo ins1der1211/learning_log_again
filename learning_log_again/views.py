5adfe03edac42f059fcdc452099e56db746cd08b
roman <yummison44@gmail.com>
2023-03-25 19:18:27 +0100

    implemented add new topic

--- learning_log_again/views.py
+++ learning_log_again/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,14 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_log_again/topic.html', context)
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_log_again:topics')
+    context = {'form': form}
+    return render(request, 'learning_log_again/new_topic.html', context)
+
