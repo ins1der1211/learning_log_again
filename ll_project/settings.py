b79a939dc96e5367e4bee8261bdde0d39fb2791f
roman <yummison44@gmail.com>
2023-03-29 11:24:23 +0200

    implemented registration and logout for users

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -127,4 +127,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'learning_log_again:index'
+LOGOUT_REDIRECT_URL = 'learning_log_again:index'
 
